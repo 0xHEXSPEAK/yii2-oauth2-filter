17f09ccf87ff74303cf0f2dd8a21db6d619f4823
denish <denishorobtsov@gmail.com>
2017-03-23 23:46:13 +0300

    Added stub entrypoint.

---
+++ 0xhexspeak/oauth-filter/src/Module.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace hexspeak\OauthFilter;
+
+use yii\base\Module
+
+class Module extends Module
+{
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+    }
+}
